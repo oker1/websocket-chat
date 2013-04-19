c0e865c9741e3bc4e9682b01d495fb20ad6c9c53
Zsolt Takács <zsolt@takacs.cc>
2013-04-19 18:17:13 +0200

    configurable port, whitespace cleanup

--- erlang/src/cset_app.erl
+++ erlang/src/cset_app.erl
@@ -13,17 +13,23 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-	Dispatch = [
-		{'_', [
-			{[], cset_handler, []}
-		]}
-	],
-	{ok, _} = cowboy:start_listener(cset_websocket_listener, 100,
-		cowboy_tcp_transport, [{port, 1337}],
-    	cowboy_http_protocol, [{dispatch, Dispatch}]
-    ),
-    cset_server:start_link(),
-    cset_sup:start_link().
+  Dispatch = [
+    {'_', [
+      {[], cset_handler, []}
+    ]}
+  ],
+
+  Port = case application:get_env(cset, port) of
+    undefined   -> 1337;
+    {ok, V}     -> V
+  end,
+
+  {ok, _} = cowboy:start_listener(cset_websocket_listener, 100,
+    cowboy_tcp_transport, [{port, Port}],
+    cowboy_http_protocol, [{dispatch, Dispatch}]
+  ),
+  cset_server:start_link(),
+  cset_sup:start_link().
 
 stop(_State) ->
     ok.
--- erlang/src/cset_handler.erl
+++ erlang/src/cset_handler.erl
@@ -5,41 +5,41 @@
 
 -export([init/3, handle/2, terminate/2]).
 -export([websocket_init/3, websocket_handle/3,
-	websocket_info/3, websocket_terminate/3]).
+  websocket_info/3, websocket_terminate/3]).
 
 init({_Any, http}, Req, []) ->
-	case cowboy_http_req:header('Upgrade', Req) of
-		{undefined, Req2} -> {ok, Req2, undefined};
-		{<<"websocket">>, _Req2} -> {upgrade, protocol, cowboy_http_websocket};
-		{<<"WebSocket">>, _Req2} -> {upgrade, protocol, cowboy_http_websocket}
-	end.
+  case cowboy_http_req:header('Upgrade', Req) of
+    {undefined, Req2} -> {ok, Req2, undefined};
+    {<<"websocket">>, _Req2} -> {upgrade, protocol, cowboy_http_websocket};
+    {<<"WebSocket">>, _Req2} -> {upgrade, protocol, cowboy_http_websocket}
+  end.
 
 handle(Req, State) ->
-	{ok, Req2} = cowboy_http_req:reply(200, [{'Content-Type', <<"text/html">>}],
+  {ok, Req2} = cowboy_http_req:reply(200, [{'Content-Type', <<"text/html">>}],
 <<"Hi!">>, Req),
-	{ok, Req2, State}.
+  {ok, Req2, State}.
 
 terminate(_Req, _State) ->
-	ok.
+  ok.
 
 websocket_init(_Any, Req, []) ->
-	%timer:send_interval(1000, "{}"),
-	cset_server:register(self()),
-	Req2 = cowboy_http_req:compact(Req),
-	{ok, Req2, undefined, hibernate}.
+  %timer:send_interval(1000, "{}"),
+  cset_server:register(self()),
+  Req2 = cowboy_http_req:compact(Req),
+  {ok, Req2, undefined, hibernate}.
 
 websocket_handle({text, Msg}, Req, State) ->
-	cset_server:chatMessage(Msg, self()),
+  cset_server:chatMessage(Msg, self()),
     {ok, Req, State};
 websocket_handle(_Any, Req, State) ->
-	{ok, Req, State}.
+  {ok, Req, State}.
 
 websocket_info({chatMessage, Msg}, Req, State) ->
     {reply, {text, Msg}, Req, State, hibernate};
 websocket_info(tick, Req, State) ->
-	{reply, {text, <<"Tick">>}, Req, State, hibernate};
+  {reply, {text, <<"Tick">>}, Req, State, hibernate};
 websocket_info(_Info, Req, State) ->
-	{ok, Req, State, hibernate}.
+  {ok, Req, State, hibernate}.
 
 websocket_terminate(_Reason, _Req, _State) ->
-	ok.
+  ok.
